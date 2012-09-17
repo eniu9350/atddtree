0d846c8b848be576fa1af4709f77a21cb3422c05
niujin <niujin@gmail.com>
2012-09-17 21:47:14 +0800

    init

---
+++ include/atddtree.h
@@ -0,0 +1,17 @@
+#ifndef _ATDDTREE_ATDDTREE_H_
+#define _ATDDTREE_ATDDTREE_H_
+
+/* 
+Arrival Time Distribution Driven Tree
+
+Inspired by "Distribution Driven Binary Tree Balancing with R-Trees"
+*/
+
+typedef struct _ddtree	{
+	
+}ddtree;
+
+
+
+#endif
+
